95efa2ee8e404b3e92d0c83fa3463629e2dae5dd
Ehtesham-19 <mehteshamdeveloper@gmail.com>
2025-08-13 14:50:09 +0500

    this is new committed

--- control flow/Assignment1/ex1.js
+++ control flow/Assignment1/ex1.js
@@ -1,14 +1,14 @@
 function Grade(marks) {
-    if (marks>90||marks==90) {
+    if (marks>90&&marks==90) {
         console.log("A")
-    } else if (marks>80<90) {
+    } else if (marks>80 &&marks<90) {
         console.log("B")
-    } else if (marks>70<79) {
+    } else if (marks>70 &&marks<79) {
         console.log("C")
-    } else if (marks>60<69) {
+    } else if (marks>60 &&marks<69) {
         
     } else {
         console.log("F")
     }
 }
-Grade(80)
+Grade(50)
---
+++ control flow/Assignment1/ex2.js
@@ -0,0 +1,11 @@
+function greet(hour) {
+    if (hour>0 && hour<12) {
+        console.log("good morning")
+    } else if (hour>12&& hour<18) {
+        console.log("good after noon")
+    } else if (hour>18 && hour<23) {
+        console.log("good night")
+    }
+    
+}
+greet(21)
